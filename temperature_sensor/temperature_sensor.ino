571948345d090c85d5540233a531fb47976f90bc
Autumn Rogers <arogers@westridge.org>
2021-05-04 11:08:36 -0700

    Temperature sensor code, requires installing Adafruit DHT sensor library.

---
+++ temperature_sensor/temperature_sensor.ino
@@ -0,0 +1,26 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("%  Temperature: "));
+  Serial.println(t);
+}
